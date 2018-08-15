b8980b0efa4d1acfa98fb58c2f95fd928b0baca5
Saverio Perugini <saverio@udayton.edu>
2018-08-15 17:21:32 -0400

    Updated header block in src/parareal.jl.

--- src/parareal.jl
+++ src/parareal.jl
@@ -1,5 +1,5 @@
 # parareal.jl: Parareal Algorithm Implementation and Simulation in Julia
-# Copyright (C) 2018  Tyler M. Mastay
+# Copyright (C) 2018  Tyler M. Mastay and Saverio Perugini
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
